7ab665f0c66ca5edf316f0be7645395fc68904e4
Jibin <jibingallistus@gmail.com>
2022-09-17 19:43:10 +0400

    Create operate function

--- script.js
+++ script.js
@@ -19,3 +19,18 @@ function divide(num1,num2) {
     return num1  / num2;
 }
 
+
+function operate (operation, num1, num2  ) {
+
+    const answer = 0;
+
+    switch(operation) {
+
+        case '+': answer = add(num1, num2); break;
+        case '-': answer = subtract(num1, num2); break;
+        case '*': answer = multiply(num1, num2); break;
+        case '/': answer = divide(num1, num2); break;
+
+    }
+
+}
